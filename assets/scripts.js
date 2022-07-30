bf43a02c55c9a1a2c62556855b42fbe78fe91265
Guilherme Marciano Goncalves <225587@SAMBA.SLT.IFSP.EDU.BR>
2022-07-29 22:39:28 -0300

    Implementando contador regressivo

--- assets/scripts.js
+++ assets/scripts.js
@@ -26,81 +26,55 @@ var cancel = setInterval(incrementSeconds, 1000);
 
 */
 
+document.getElementById("SetMinutes").addEventListener("change", () => {
+  let displayedMinute = document.getElementById("SetMinutes").value;
+  if (displayedMinute < 10) {
+    displayedMinute = 0 + displayedMinute;
+  }
 
+  if (displayedMinute > 0 && displayedMinute < 60) {
+    document.getElementById("DisplayMinute").innerText = displayedMinute;
+  }
+});
 
-document.getElementById("SetMinutes").addEventListener("change", ()=>
-{
+document.getElementById("SetSeconds").addEventListener("change", () => {
+  let displayedSecond = document.getElementById("SetSeconds").value;
+  if (displayedSecond < 10) {
+    displayedSecond = 0 + displayedSecond;
+  }
+  if (displayedSecond > 0 && displayedSecond < 60) {
+    document.getElementById("DisplaySecond").innerText = displayedSecond;
+}
+});
 
-     let displayedMinute = document.getElementById("SetMinutes").value
-     if(displayedMinute<10)
-     {
-        displayedMinute = 0 + displayedMinute
-     }
-     
-     if(displayedMinute>0 && displayedMinute<60){
-     document.getElementById("DisplayMinute").innerText = displayedMinute
-     }
-})
-
-
-
-document.getElementById("SetSeconds").addEventListener("change", ()=>
-{
-
-     let displayedSecond = document.getElementById("SetSeconds").value
-     if(displayedSecond<10)
-     {
-        displayedSecond = 0 + displayedSecond
-     }     
-     if(displayedSecond>0&&displayedSecond<60)
-     {
-     document.getElementById("DisplaySecond").innerText = displayedSecond
-     }
-})
-
-
-document.getElementById("SubmitButton").addEventListener("click", ()=>
-{
-    document.getElementById("SetSeconds").disabled = true
-    document.getElementById("SetMinutes").disabled = true
-    document.getElementById("SubmitButton").disabled = true
-
-    CountDown();
+document.getElementById("SubmitButton").addEventListener("click", () => {
+    document.getElementById("SetSeconds").disabled = true;
+    document.getElementById("SetMinutes").disabled = true;
+    document.getElementById("SubmitButton").disabled = true;
     
-})
- 
+    CountDown()
+    
+});
 
-    function CountDown(){
+function CountDown() {
 
+    window.setInterval(()=>{
+        let displayedSecond = document.getElementById("DisplaySecond").textContent;
+        let displayedMinute = document.getElementById("DisplaySecond").value;
 
-        let displayedSecond = document.getElementById("SetSeconds").value
-        let displayedMinute = document.getElementById("SetMinutes").value
+    
+    console.log(displayedSecond);
+    console.log(Number(displayedSecond));
+    displayedSecond--;
+    document.getElementById("DisplaySecond").innerText = displayedSecond;    
+    }, 1000)
+    
+    
 
+}
 
-        while(displayedMinute>=0){
-        while(displayedSecond>=0){
-
-
-                console.log("Cheguei")
-
-            }   
-        }
-
-
-        
-    }
-
-
-
-
-
-
-document.getElementById("ResetButton").addEventListener("click", ()=>
-{
-    document.getElementById("SetSeconds").disabled = false 
-    document.getElementById("SetMinutes").disabled = false    
-    document.getElementById("SubmitButton").disabled = false    
-})
-
-
-
+document.getElementById("ResetButton").addEventListener("click", () => {
+  document.getElementById("SetSeconds").disabled = false;
+  document.getElementById("SetMinutes").disabled = false;
+  document.getElementById("SubmitButton").disabled = false;
+});
